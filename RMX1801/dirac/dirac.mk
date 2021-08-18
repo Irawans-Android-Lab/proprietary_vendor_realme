0817908f12a4d896049c6749a315692394bda1c4
sb6596 <shubhamprince111@gmail.com>
2021-08-18 04:32:07 +0000

    RMX1801: Add Dirac Blobs
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>

---
+++ RMX1801/dirac/dirac.mk
@@ -0,0 +1,4 @@
+PRODUCT_COPY_FILES += \
+    vendor/realme/RMX1801/dirac/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
+    vendor/realme/RMX1801/dirac/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
+    vendor/realme/RMX1801/dirac/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
